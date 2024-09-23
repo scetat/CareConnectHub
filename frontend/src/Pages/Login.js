1779b0ecd136aaa12de408777464189ec9fecf19
Naishal23 <naishalrajput01@gmail.com>
2024-09-23 19:46:54 -0400

    add login fileds

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -10,10 +10,39 @@ const Login = () => {
       <Header />
 
       <div className="logo-container">
+        {/* Logo */}
+        <img
+          src="/Assests/logo.png"
+          alt="Care Connect Hub Logo"
+          className="logo"
+        />
         <h1>CARE CONNECT HUB</h1>
       </div>
 
-      
+      <form className="login-form">
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type="email"
+            placeholder="example@you.com"
+            className="form-input"
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            placeholder="Password"
+            className="form-input"
+          />
+        </div>
+
+        <button type="submit" className="login-button">
+          Log In
+        </button>
+      </form>
+
       <div className="signup-link">
         <p>Don’t Have an Account? <a href="/signup">Sign Up</a></p>
       </div>
