3e80858dcdd4400313ab51013ed98e896a49eef9
ChiraGg77 <chiragcanada90@gmail.com>
2024-09-25 23:52:34 -0400

    Responsive Done For All screen

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,21 +1,61 @@
-// Header.js
-import React from 'react';
+import React, { useState } from 'react';
 import '../../css/style.css';
 
 const Header = () => {
+  const [activeLink, setActiveLink] = useState('home'); // Default active link
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // State for hamburger menu
+
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+    setIsMenuOpen(false); // Close menu when a link is clicked
+  };
+
   return (
     <header className="header">
       <div className="header__logo">
         <img src="/logo.png" alt="Care Connect Hub" />
       </div>
-      <nav className="header__nav">
-        <a href="/home" className="header__link header__link--active">HOME</a>
-        <a href="/events" className="header__link">EVENT</a>
-        <a href="/caregiver" className="header__link">CAREGIVER</a>
-        <a href="/booking" className="header__link">BOOKING</a>
-        <a href="/about" className="header__link">ABOUT US</a>
-      </nav>
+      <div className={`header__nav ${isMenuOpen ? 'open' : ''}`}>
+        <a
+          href="/home"
+          className={`header__link ${activeLink === 'home' ? 'header__link--active' : ''}`}
+          onClick={() => handleLinkClick('home')}
+        >
+          HOME
+        </a>
+        <a
+          href="/events"
+          className={`header__link ${activeLink === 'events' ? 'header__link--active' : ''}`}
+          onClick={() => handleLinkClick('events')}
+        >
+          EVENT
+        </a>
+        <a
+          href="/caregiver"
+          className={`header__link ${activeLink === 'caregiver' ? 'header__link--active' : ''}`}
+          onClick={() => handleLinkClick('caregiver')}
+        >
+          CAREGIVER
+        </a>
+        <a
+          href="/booking"
+          className={`header__link ${activeLink === 'booking' ? 'header__link--active' : ''}`}
+          onClick={() => handleLinkClick('booking')}
+        >
+          BOOKING
+        </a>
+        <a
+          href="/about"
+          className={`header__link ${activeLink === 'about' ? 'header__link--active' : ''}`}
+          onClick={() => handleLinkClick('about')}
+        >
+          ABOUT US
+        </a>
+      </div>
       <button className="header__signup">Sign-up</button>
+      <button className="header__hamburger" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+        &#9776; {/* Hamburger icon */}
+      </button>
     </header>
   );
 };
