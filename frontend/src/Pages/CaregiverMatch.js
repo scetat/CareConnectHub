e66340e80d64ddadaf30cd3f9502c525fca3b77c
Naishal23 <naishalrajput01@gmail.com>
2024-10-20 16:07:36 -0400

    bug fix

---
+++ frontend/src/Pages/CaregiverMatch.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import '../css/caregivermatch.css';
+
+const CaregiverMatch = () => {
+  return (
+    <div className="caregiver-container">
+      <header>
+        <h1>Caregiver Match</h1>
+        <div className="search-bar">
+          <input type="text" placeholder="Search Caregivers..." />
+          <button className="search-btn">Search</button>
+        </div>
+      </header>
+
+      <div className="filter-results-container">
+        {/* Filter Section */}
+        <div className="filter-section">
+          <h2>Filter By</h2>
+          <div className="filter-group">
+            <label htmlFor="experience">Experience</label>
+            <select id="experience">
+              <option value="1-6">1-6 Years</option>
+              <option value="6-10">6-10 Years</option>
+              <option value="10+">10+ Years</option>
+            </select>
+          </div>
+
+          <div className="filter-group">
+            <label htmlFor="location">Location</label>
+            <input type="text" id="location" placeholder="Enter Zip Code" />
+          </div>
+
+          <div className="filter-group">
+            <label htmlFor="rating">Rating</label>
+            <select id="rating">
+              <option value="4-star">4 Star</option>
+              <option value="5-star">5 Star</option>
+            </select>
+          </div>
+
+          <div className="filter-group">
+            <label htmlFor="availability">Availability</label>
+            <select id="availability">
+              <option value="weekdays">Weekdays</option>
+              <option value="weekends">Weekends</option>
+            </select>
+          </div>
+        </div>
+
+        {/* Results Section */}
+        <div className="results-section">
+          <h2>Results</h2>
+          <div class="card-container">
+          <div className="result-card">
+            <div className="result-image"></div>
+            <div className="result-info">
+              <h3>John Doe</h3>
+              <p><strong>Rating:</strong> ★★★★★</p>
+              <p>Available: Weekdays</p>
+              <button>More info</button>
+            </div>
+          </div>
+          <div className="result-card">
+            <div className="result-image"></div>
+            <div className="result-info">
+              <h3>John Doe</h3>
+              <p><strong>Rating:</strong> ★★★★★</p>
+              <p>Available: Weekdays</p>
+              <button>More info</button>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CaregiverMatch;
