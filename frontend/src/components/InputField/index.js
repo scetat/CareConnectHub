6a9abced67d1c28f4a8f9046e8664b5eedd66e04
Nnamdi Ogbu <ogbunnamdi@gmail.com>
2024-12-12 11:14:31 -0500

    fix manage profile

--- frontend/src/components/InputField/index.js
+++ frontend/src/components/InputField/index.js
@@ -1,7 +1,7 @@
-const InputField = ({ label, type, value, onChange }) => (
+const InputField = ({ label, type, value, onChange, disabled = false }) => (
   <div className="input-field">
     <label>{label}</label>
-    <input type={type} value={value} onChange={onChange} />
+    <input type={type} value={value} onChange={onChange} disabled={disabled} />
   </div>
 );
 
